2c5aa9197b5a1417b07ea8e99a1a6578ca0cf583
Mike Stott <mike@topi.eu>
2024-12-10 16:21:21 +0000

    wip

---
+++ jsonparser/javascript/helpers.mjs
@@ -0,0 +1,13 @@
+export const isLeftCurlyBrace = (caracter) => caracter === "{";
+
+export const isRightCurlyBrace = (caracter) => caracter === "}";
+
+export const isDoubleQuote = (caracter) => caracter === `"`;
+
+export const isComma = (caracter) => caracter === ",";
+
+export const isColon = (caracter) => caracter === ":";
+
+export const isKey = () => true;
+
+export const isValue = () => true;
